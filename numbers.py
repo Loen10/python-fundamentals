04c5813fbf0af7db6477d322edf4a5d84fea3908
Logan Nance <ldnance10@gmail.com>
2022-10-06 18:10:08 -0600

    I dun created an initiAl coMit

---
+++ numbers.py
@@ -0,0 +1,16 @@
+import random
+
+my_integer = 1923
+second_integer = 1978
+my_rndm_year = random.randint(1974, 2022)
+print(my_rndm_year)
+time_passed = second_integer - my_integer
+print(time_passed)
+
+apple_price = 1.79
+orange_price = 1.25
+banana_price_int = 2
+banana_price_float = float(banana_price_int)
+bill = apple_price + orange_price + banana_price_int
+print(bill)
+
