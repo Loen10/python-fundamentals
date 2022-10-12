074e532d597f67ab79b4a2547170cd015b5336c7
Logan Nance <ldnance10@gmail.com>
2022-10-11 18:23:44 -0600

    add looping to guessing game

--- conditionals.py
+++ conditionals.py
@@ -2,17 +2,15 @@
 # Method evaluate data
 import random
 
-high_range = 100
-middle_number = int(high_range / 2)
-my_guess = middle_number
-guesses_count = 0
-random_number = random.randint(1, high_range)
-comparison = "equal to"
+# ask the user what the upper bound is and what their guess is
+upper_bound = int(input("What is the upper bound? "))
+random_number = random.randint(1, upper_bound)
+guess = int(input("Guess a number between 1 and {} inclusive: " \
+    .format(upper_bound)))
 
-#evaluate the random number and compare it to the middle number
-if my_guess > random_number:
-    comparison = "higher than"
-elif my_guess < random_number:
-    comparison = "lower than"
+while guess != random_number:
+    print("Nope, try again")
+    guess = int(input("Guess a number between 1 and {} inclusive: " \
+        .format(upper_bound)))
 
-print("{} is {} {}".format(my_guess, comparison, random_number))
+print("Correct, you win!")
