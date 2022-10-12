4d53810654a7760230ec340aeaec3cedcb3f39e3
Logan Nance <ldnance10@gmail.com>
2022-10-11 18:53:35 -0600

    counted guesses

--- conditionals.py
+++ conditionals.py
@@ -3,14 +3,31 @@
 import random
 
 # ask the user what the upper bound is and what their guess is
-upper_bound = int(input("What is the upper bound? "))
+
+invalid_upper_bound = True
+while invalid_upper_bound:
+    upper_bound = input("What is the upper bound? ")
+    
+    if upper_bound.isdigit():
+        invalid_upper_bound = False
+        upper_bound = int(upper_bound)
+    else:
+        print("Please enter a whole number")
+
 random_number = random.randint(1, upper_bound)
-guess = int(input("Guess a number between 1 and {} inclusive: " \
-    .format(upper_bound)))
+guess_count = 1
+guess = input("Guess a number between 1 and {} inclusive: " \
+    .format(upper_bound))
+
+if guess.isdigit():
+    guess = int(guess)
 
 while guess != random_number:
     print("Nope, try again")
-    guess = int(input("Guess a number between 1 and {} inclusive: " \
-        .format(upper_bound)))
+    guess = input("Guess a number between 1 and {} inclusive: " \
+        .format(upper_bound))
+    
+    if guess.isdigit():
+        guess = int(guess)
 
-print("Correct, you win!")
+print("Correct, you won with {} guesses".format(guess_count))
